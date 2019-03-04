9d8d03471078dbe8f28db067ee8fa11685b49861
janek <janek93.wroblewski@gmail.com>
2019-03-04 13:09:19 +0000

    first commit

---
+++ app/src/main/java/com/example/recyclerview/object.kt
@@ -0,0 +1,33 @@
+package com.example.recyclerview
+
+
+
+
+    data class ArrayListf (val name1: String, val name2: String) {
+
+
+
+
+
+
+
+    }
+
+fun klasa() {
+
+    var kdkfdf = ArrayListf("imie1", "imie2")
+
+    print(kdkfdf)
+
+}
+
+
+
+
+
+
+
+
+
+
+
